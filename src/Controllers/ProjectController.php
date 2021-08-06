dae1d8126d89c95a71e785a66cc4ca063d39d91a
CallMeDKay <s3812649@rmit.edu.vn>
2021-08-06 22:06:11 +0700

    :memo: add MIT license

--- src/Controllers/ProjectController.php
+++ src/Controllers/ProjectController.php
@@ -26,8 +26,32 @@ class ProjectController {
     ]);
   }
 
-  public function add() {
-    ServerLogger::log("Form posted");
+  public function add(
+    string $project_name,
+    string $subtype,
+    string $current_status,
+    ?float $capacity_mw,
+    ?int $year_of_completion,
+    ?string $country_list_of_sponsor_developer,
+    ?string $sponsor_developer_company,
+    ?string $country_list_of_lender_financier,
+    ?string $lender_financier_company,
+    ?string $country_list_of_construction_epc,
+    ?string $construction_company_epc_participant,
+    string $country,
+    ?string $province_state,
+    string $district,
+    ?string $tributary,
+    float $latitude,
+    float $longitude,
+    ?string $proximity,
+    ?float $avg_annual_output_mwh,
+    ?string $data_source,
+    ?string $announce_more_information,
+    ?string $link,
+    ?string $latest_update
+  ) {
+    ServerLogger::log("Form posted (project's name): " . $project_name);
     header("Location: " . getenv("DEPLOY_URL") ? getenv("DEPLOY_URL") : "http://localhost:8080");
   }
 }
